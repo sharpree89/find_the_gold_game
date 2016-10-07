e28bbacc004798dd57be7b0ff003ceedefa5739a
Preeya Sharma <sharpree89@gmail.com>
2016-10-07 13:30:26 -0700

    first commit

--- NGModule.cs
+++ NGModule.cs
@@ -1,6 +1,7 @@
 using Nancy;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
+
 namespace NG1
 {
     public class NGModule : NancyModule
@@ -15,49 +16,45 @@ namespace NG1
                 ViewBag.log = "";
             }
             ViewBag.gold = Session["gold"];
-            ViewBag.log = Session["log"];
+            ViewBag.log = Session["log"]; 
             return View["NG.sshtml"];
             });
 
             Post("/process_money", args =>
             {
                 Random rnd = new Random();
-                string building = Request.Form.building;     
+                string building = Request.Form["building"];     
 
                 if(building == "farm"){
-                    Console.WriteLine("inside the farm");
                     int farmGold = (int)rnd.Next(10,21);
                     Session["gold"] = (int)Session["gold"] + farmGold;
-                    Session["log"] += $"<p> You earned {farmGold} gold from the farm!</p>";
+                    Session["log"] += $"<p> You found {farmGold} gold on the farm!</p>";
                 } 
                 if(building == "cave"){
-                    Console.WriteLine("inside the cave");
                     int caveGold = (int)rnd.Next(5,11);
                     Session["gold"] = (int)Session["gold"] + caveGold;
-                    Session["log"] += $"<p> You earned {caveGold} gold from the cave!</p>";
+                    Session["log"] += $"<p> You found {caveGold} gold in the cave!</p>";
                 }
                 if(building == "house"){
-                    Console.WriteLine("inside the house");
                     int houseGold = (int)rnd.Next(2,6);
                     Session["gold"] = (int)Session["gold"] + houseGold;
-                    Session["log"] += $"<p> You earned {houseGold} gold from the house!</p>";
+                    Session["log"] += $"<p> You found {houseGold} gold in the house!</p>";
                 }
                 if(building == "casino"){
-                    Console.WriteLine("inside the casino");
                     int casinoGold = (int)rnd.Next(-50,51);
                     if(casinoGold > 0)    
                     {
                         Session["gold"] = (int)Session["gold"] + casinoGold;
-                        Session["log"] += $"<p> You won {casinoGold} gold from the casino!</p>";
+                        Session["log"] += $"<p> You found {casinoGold} gold in the casino!</p>";
                     }
                     else if(casinoGold < 0)    
                     {
                         Session["gold"] = (int)Session["gold"] + casinoGold;
-                        Session["log"] += $"<p> You lost {casinoGold} gold from the casino!</p>";
+                        Session["log"] += $"<p> You lost {casinoGold} gold in the casino!</p>";
                     }
                     else if(casinoGold == 0)
                     {
-                        Session["log"] += $"<p> You broke even! 0 gold won/lost at the casino. </p>";
+                        Session["log"] += $"<p> You broke even - {casinoGold} gold found at the casino! </p>";
                     }
                 }
                 return Response.AsRedirect("/");
